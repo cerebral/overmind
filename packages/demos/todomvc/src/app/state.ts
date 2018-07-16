6c1d3143f4d2eebea5c98ca780458afc2f86822c
christianalfoni <christianalfoni@gmail.com>
2018-07-16 22:03:16 +0200

    feat(overmind-devtools): components tab

--- packages/demos/todomvc/src/app/state.ts
+++ packages/demos/todomvc/src/app/state.ts
@@ -1,4 +1,5 @@
 export type Todo = {
+  id: string
   title: string
   completed: boolean
 }
--- packages/demos/todomvc/src/components/Todos/index.tsx
+++ packages/demos/todomvc/src/components/Todos/index.tsx
@@ -5,7 +5,10 @@ import Todo from '../Todo'
 
 const Todos: React.SFC<Connect> = ({ appState }) => (
   <List>
-    {appState.todos.map((todo) => <Todo key={todo.title} todo={todo} />)}
+    {appState.todos.map(
+      (todo, index) =>
+        console.log('todo ID', todo.id) || <Todo key={todo.id} todo={todo} />
+    )}
   </List>
 )
 
