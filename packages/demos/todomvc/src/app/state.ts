8aa0d3c27e1e2f9818683c56f0bf494ebb8fa534
christianalfoni <christianalfoni@gmail.com>
2018-07-20 23:17:39 +0200

    feat(overmind): computed state now in state and shown in devtools

--- packages/demos/todomvc/src/app/state.ts
+++ packages/demos/todomvc/src/app/state.ts
@@ -1,4 +1,4 @@
-import { derived } from 'overmind'
+import { derived, computed } from 'overmind'
 
 export type Todo = {
   id: string
@@ -10,12 +10,14 @@ export type State = {
   todos: Todo[]
   newTodoTitle: string
   count: number
+  testCount: (foo: number) => number
 }
 
 const state: State = {
   todos: [],
   count: derived((state: State) => state.todos.length),
   newTodoTitle: '',
+  testCount: computed((foo: number) => (state: State) => state.count + foo),
 }
 
 export default state
