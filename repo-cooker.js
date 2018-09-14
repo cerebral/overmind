5b2710edff2845ac9ee6a666f7143eaa4040d9b6
Gaspard Bucher <gaspard@lucidogen.io>
2018-09-14 08:50:36 +0200

    chore(repo-cooker): fix settings to allow github login

--- repo-cooker.js
+++ repo-cooker.js
@@ -1,5 +1,7 @@
 import { Cooker } from 'repo-cooker'
 
+process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
+
 export const cooker = Cooker(process.argv, {
   devtools: {
     host: 'localhost:8787, reconnect: false',
