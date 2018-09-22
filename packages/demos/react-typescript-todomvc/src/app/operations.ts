e82a8b3181583aedee8b6bab205f2e825b0cd20e
christianalfoni <christianalfoni@gmail.com>
2018-09-22 19:50:13 +0200

    refactor(demo): fix typing to new name

--- packages/demos/react-typescript-todomvc/src/app/operations.ts
+++ packages/demos/react-typescript-todomvc/src/app/operations.ts
@@ -5,5 +5,5 @@ type ChangeEvent = React.ChangeEvent<HTMLInputElement>
 export const getEventValue: Operation.Map<ChangeEvent, string> = (_, event) =>
   event.currentTarget.value
 
-export const preventEventDefault: Operation.Do<React.FormEvent> = (_, event) =>
+export const preventEventDefault: Operation.Run<React.FormEvent> = (_, event) =>
   event.preventDefault()
