fdbcd20a7c0ea1647542871f0b1eef7683588bce
christianalfoni <christianalfoni@gmail.com>
2018-10-01 20:17:56 +0200

    feat(overmind): make derived work as plain function and remove computed
    BREAKING CHANGE:
    new derived API and removed computed

--- packages/overmind-website/examples/api/derive.ts
+++ packages/overmind-website/examples/api/derive.ts
@@ -2,51 +2,32 @@ export default (ts) =>
   ts
     ? [
         {
-          fileName: 'derived.ts',
-          code: `
-import { Derive } from 'overmind'
-import { Item } from './state'
-
-export const completedItems: Derive<Item[]> = state =>
-  state.items.filter(item => item.completed)
-
-      `,
-        },
-        {
           fileName: 'app/state.ts',
           code: `
-import { derive } from 'overmind'
-import * as derived from './derived'
+import { Derive } from 'overmind'
 
 export type Item = {
-title: string
-completed: boolean
+  title: string
+  completed: boolean
 }
 
 export const items: Item[] = []
 
-export const completedItems: Item[] = derive(derived.completedItems)
+export const completedItems: Derive<Item[]> = state =>
+  state.items.filter(item => item.completed)
     `,
         },
       ]
     : [
         {
-          fileName: 'derived.js',
-          code: `
-export const completedItems = state =>
-  state.items.filter(item => item.completed)
-
-      `,
-        },
-        {
           fileName: 'app/state.js',
           code: `
-import { derive } from 'overmind'
 import * as derived from './derived'
 
 export const items = []
 
-export const completedItems = derive(derived.completedItems)
+export const completedItems = state =>
+  state.items.filter(item => item.completed)
     `,
         },
       ]
