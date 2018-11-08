e411b1fd8cf98077786a97d0681a42ab8d9cb4d5
christianalfoni <christianalfoni@gmail.com>
2018-11-08 20:30:23 +0100

    feat(overmind-devtools): add devtool tracking to all operators

--- packages/demos/react-typescript-todomvc/src/app/actions.ts
+++ packages/demos/react-typescript-todomvc/src/app/actions.ts
@@ -1,17 +1,41 @@
 import * as React from 'react'
-import { Action } from 'overmind'
+import {
+  Action,
+  pipe,
+  Operator,
+  map,
+  mutate,
+  when,
+  run,
+  wait,
+  fork,
+  debounce,
+  filter,
+  forEach,
+} from 'overmind'
 import { Todo } from './state'
 
 type ChangeEvent = React.ChangeEvent<HTMLInputElement>
 
 let nextTodoId = 0
 
+export const changeNewTodoTitle: Operator<ChangeEvent, any> = pipe(
+  map(({ value }) => value.target.value),
+  mutate(({ state, value }) => (state.newTodoTitle = value)),
+  map(() => ['foo', 'bar', 'baz']),
+  forEach(run(() => {})),
+  filter(() => true),
+  debounce(200)
+)
+
+/*
 export const changeNewTodoTitle: Action<ChangeEvent> = ({
   value: event,
   state,
 }) => {
   state.newTodoTitle = event.target.value
 }
+*/
 
 export const addTodo: Action = ({ state }) => {
   state.todos.unshift({
