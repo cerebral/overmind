e63abcd8b08ea9236005773d249052b1128854a5
christianalfoni <christianalfoni@gmail.com>
2018-12-04 20:45:15 +0100

    feat(overmind): lazy config

---
+++ packages/overmind-website/examples/api/config_lazy.ts
@@ -0,0 +1,48 @@
+export default (ts) =>
+  ts
+    ? [
+        {
+          fileName: 'app.ts',
+          code: `
+import { Overmind, TConfig } from 'overmind'
+import { lazy } from 'overmind/config'
+import { Config as ModuleAConfig } from './moduleA'
+import { Config as ModuleBConfig } from './moduleB'
+
+const config = lazy({
+  moduleA: (): Promise<ModuleAConfig> => import('./moduleA'),
+  moduleB: (): Promise<ModuleBConfig> => import('./moduleB)
+})
+
+declare module 'overmind' {
+  interface IConfig extends TConfig<typeof config> {}
+}
+
+const app = new Overmind(config)
+
+app.actions.lazy.loadConfig('moduleA')
+
+export default app
+`,
+        },
+      ]
+    : [
+        {
+          fileName: 'app/index.js',
+          code: `
+import { Overmind } from 'overmind'
+import { lazy } from 'overmind/config'
+
+const config = lazy({
+  moduleA: () => import('./moduleA'),
+  moduleB: () => import('./moduleB)
+})
+
+const app = new Overmind(config)
+
+app.actions.lazy.loadConfig('moduleA')
+
+export default app
+`,
+        },
+      ]
