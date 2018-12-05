28be363c204932772a4221c8651752cf4fbfb4c9
christianalfoni <christianalfoni@gmail.com>
2018-12-05 13:28:24 +0100

    docs(website): add missing views in routing guide

--- packages/overmind-website/examples/guide/routing/users.ts
+++ packages/overmind-website/examples/guide/routing/users.ts
@@ -1,4 +1,102 @@
 const javascript = {
+  components: [
+    {
+      fileName: 'components/App.js',
+      target: 'jsx',
+      code: `
+import { h, useOvermind } from 'overmind-components' 
+import Users from './Users'
+
+const App = () => {
+  const { state } = useOvermind()
+
+  return (
+    <div class="container">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/users">Users</a>
+      </nav>
+      {state.currentPage === 'home' ? <h1>Hello world!</h1> : null}
+      {state.currentPage === 'users' ? <Users /> : null}
+    </div>
+  )
+}
+
+export default App
+    `,
+    },
+    {
+      fileName: 'components/Users.js',
+      target: 'jsx',
+      code: `
+import { h, useOvermind } from 'overmind-components'
+import UserModal from './UserModal'
+
+const Users = () => {
+  const { state } = useOvermind()
+
+  return (
+    <div class="content">
+      {state.isLoadingUsers ? (
+        <h4>Loading users...</h4>
+      ) : (
+        <ul>
+          {state.users.map(user => (
+            <li key={user.id}>
+              <a href={"/users/" + user.id}>{user.name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {state.modalUser ? <UserModal /> : null}
+    </div>
+  )
+}
+
+export default Users
+    `,
+    },
+    {
+      fileName: 'components/UserModal.js',
+      target: 'jsx',
+      code: `
+import { h, useOvermind } from 'overmind-components'
+
+const UserModal = () => {
+  const { state } = useOvermind()
+  const modalUser = state.modalUser
+  const currentUserModalTabIndex = state.currentUserModalTabIndex
+
+  return (
+    <a href="/users" class="backdrop">
+      <div class="modal">
+        {overmind.state.isLoadingUserDetails ? (
+          <h4>Loading user details...</h4>
+        ) : (
+          <div>
+            <h4>{modalUser.name}</h4>
+            <h6>{modalUser.details.email}</h6>
+            <nav>
+              <a href={"/users/" + modalUser.id + "?tab=0"}>bio</a>
+              <a href={"/users/" + modalUser.id + "?tab=1"}>address</a>
+            </nav>
+            {currentUserModalTabIndex === 0 ? (
+              <div class="tab-content">{modalUser.details.bio}</div>
+            ) : null}
+            {currentUserModalTabIndex === 1 ? (
+              <div class="tab-content">{modalUser.details.address}</div>
+            ) : null}
+          </div>
+        )}
+      </div>
+    </a>
+  )
+}
+
+export default UserModal
+    `,
+    },
+  ],
   react: [
     {
       fileName: 'components/App.js',
@@ -91,9 +189,187 @@ export default connect(UserModal)
     `,
     },
   ],
+  vue: [
+    {
+      fileName: 'components/app-component.vue',
+      code: `
+<template>
+  <div class="container">
+    <nav>
+      <a href="/">Home</a>
+      <a href="/users">Users</a>
+    </nav>
+    <h1 v-if="overmind.state.currentPage === 'home'">Hello world!</h1>
+    <users-list v-if="overmind.state.currentPage === 'users'"></users-list>
+  </div>
+</template>
+<script>
+import { connect } from '../app'
+
+export default connect({})
+</script>
+    `,
+    },
+    {
+      fileName: 'components/users-list.vue',
+      code: `
+<template>
+  <div class="content">
+    <h4 v-if="overmind.state.isLoadingUsers">Loading users...</h4>
+    <ul v-else>
+      <li v-for="user in overmind.state.users" :key="user.id">
+        <a href={"/users/" + user.id}>{{user.name}}</a>
+      </li>
+    </ul>
+    <user-modal v-if="overmind.state.userModal"></user-modal>
+  </div>
+</template>
+<script>
+import { connect } from '../app'
+
+export default connect({})
+</script>
+    `,
+    },
+    {
+      fileName: 'components/user-modal.vue',
+      code: `
+<template>
+  <a href="/users" class="backdrop">
+    <div class="modal">
+      <h4 v-if="overmind.state.isLoadingUserDetails">Loading user details...</h4>
+      <div v-else>
+        <h4>{{overmind.state.modalUser.name}}</h4>
+        <h6>{{overmind.state.modalUser.details.email}}</h6>
+        <nav>
+          <a v-bind:href="'/users/' + overmind.state.modalUser.id + '?tab=0'">bio</a>
+          <a v-bind:href="'/users/' + overmind.state.modalUser.id + '?tab=1'">address</a>
+        </nav>
+        <div
+          v-if="overmind.state.currentUserModalTabIndex === 0"
+          class="tab-content"
+        >
+          {{modalUser.details.bio}}
+        </div>
+        <div
+          v-if="overmind.state.currentUserModalTabIndex === 1"
+          class="tab-content"
+        >
+          {{modalUser.details.address}}
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+<script>
+import { connect } from '../app'
+
+export default connect({})
+</script>
+    `,
+    },
+  ],
 }
 
 const typescript = {
+  components: [
+    {
+      fileName: 'components/App.tsx',
+      target: 'jsx',
+      code: `
+import { h, Component, useOvermind } from 'overmind-components' 
+import Users from './Users'
+
+const App: Component = () => {
+  const { state } = useOvermind()
+
+  return (
+    <div class="container">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/users">Users</a>
+      </nav>
+      {state.currentPage === 'home' ? <h1>Hello world!</h1> : null}
+      {state.currentPage === 'users' ? <Users /> : null}
+    </div>
+  )
+}
+
+export default App
+    `,
+    },
+    {
+      fileName: 'components/Users.tsx',
+      target: 'jsx',
+      code: `
+import { h, Component, useOvermind } from 'overmind-components'
+import UserModal from './UserModal'
+
+const Users: Component = () => {
+  const { state } = useOvermind()
+
+  return (
+    <div class="content">
+      {state.isLoadingUsers ? (
+        <h4>Loading users...</h4>
+      ) : (
+        <ul>
+          {state.users.map(user => (
+            <li key={user.id}>
+              <a href={"/users/" + user.id}>{user.name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {state.modalUser ? <UserModal /> : null}
+    </div>
+  )
+}
+
+export default Users
+    `,
+    },
+    {
+      fileName: 'components/UserModal.tsx',
+      target: 'jsx',
+      code: `
+import { h, Component, useOvermind } from 'overmind-components'
+
+const UserModal: Component = () => {
+  const { state } = useOvermind()
+  const modalUser = state.modalUser
+  const currentUserModalTabIndex = state.currentUserModalTabIndex
+
+  return (
+    <a href="/users" class="backdrop">
+      <div class="modal">
+        {overmind.state.isLoadingUserDetails ? (
+          <h4>Loading user details...</h4>
+        ) : (
+          <div>
+            <h4>{modalUser.name}</h4>
+            <h6>{modalUser.details.email}</h6>
+            <nav>
+              <a href={"/users/" + modalUser.id + "?tab=0"}>bio</a>
+              <a href={"/users/" + modalUser.id + "?tab=1"}>address</a>
+            </nav>
+            {currentUserModalTabIndex === 0 ? (
+              <div class="tab-content">{modalUser.details.bio}</div>
+            ) : null}
+            {currentUserModalTabIndex === 1 ? (
+              <div class="tab-content">{modalUser.details.address}</div>
+            ) : null}
+          </div>
+        )}
+      </div>
+    </a>
+  )
+}
+
+export default UserModal
+    `,
+    },
+  ],
   react: [
     {
       fileName: 'components/App.tsx',
@@ -180,6 +456,100 @@ const UserModal: React.SFC<Connect> = ({ overmind }) => {
 }
 
 export default connect(UserModal)
+    `,
+    },
+  ],
+  vue: javascript.vue,
+  angular: [
+    {
+      fileName: 'components/app-component.ts',
+      code: `
+import { Component } from '@angular/core';
+import { connect } from '../app'
+
+@Component({
+  selector: 'app-component',
+  template: \`
+  <div class="container">
+    <nav>
+      <a href="/">Home</a>
+      <a href="/users">Users</a>
+    </nav>
+    <h1 *ngIf="overmind.state.currentPage === 'home'">Hello world!</h1>
+    <users-list *ngIf="overmind.state.currentPage === 'users'"></users-list>
+  </div>
+  \`
+})
+@connect()
+export class AppComponent {}
+    `,
+    },
+    {
+      fileName: 'components/users-list.ts',
+      code: `
+import { Component } from '@angular/core';
+import { connect } from '../app'
+
+@Component({
+  selector: 'users-list',
+  template: \`
+  <div class="content">
+    <h4 *ngIf="overmind.state.isLoadingUsers">Loading users...</h4>
+    <ul *ngIf="!overmind.state.isLoadingUsers">
+      <li *ngFor="let user of overmind.state.users;trackby: trackById">
+        <a href={"/users/" + user.id}>{{user.name}}</a>
+      </li>
+    </ul>
+    <user-modal *ngIf="overmind.state.userModal"></user-modal>
+  </div>
+  \`
+})
+@connect()
+export class UsersList {
+  trackById(index, user) {
+    return user.id
+  }
+}
+    `,
+    },
+    {
+      fileName: 'components/user-modal.ts',
+      code: `
+import { Component } from '@angular/core';
+import { connect } from '../app'
+
+@Component({
+  selector: 'user-modal',
+  template: \`
+  <a href="/users" class="backdrop">
+    <div class="modal">
+      <h4 *ngIf="overmind.state.isLoadingUserDetails">Loading user details...</h4>
+      <div *ngIf="!overmind.state.isLoadingUserDetails">
+        <h4>{{overmind.state.modalUser.name}}</h4>
+        <h6>{{overmind.state.modalUser.details.email}}</h6>
+        <nav>
+          <a [href]="'/users/' + overmind.state.modalUser.id + '?tab=0'">bio</a>
+          <a [href]="'/users/' + overmind.state.modalUser.id + '?tab=1'">address</a>
+        </nav>
+        <div
+          *ngIf="overmind.state.currentUserModalTabIndex === 0"
+          class="tab-content"
+        >
+          {{modalUser.details.bio}}
+        </div>
+        <div
+          *ngIf="overmind.state.currentUserModalTabIndex === 1"
+          class="tab-content"
+        >
+          {{modalUser.details.address}}
+        </div>
+      </div>
+    </div>
+  </a>
+  \`
+})
+@connect()
+export class UserModal {}
     `,
     },
   ],
