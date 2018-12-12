f66e3f404dd3eab454b5ac556959f325965b1d5d
christianalfoni <christianalfoni@gmail.com>
2018-12-12 19:35:41 +0100

    docs(website): remove reaction leftovers

--- packages/overmind-website/api/overmind.md
+++ packages/overmind-website/api/overmind.md
@@ -8,7 +8,16 @@ h(Example, { name: "api/app_initialize" })
 
 You can pass a second argument to the **App** constructor. This is an options object with the following properties:
 
-## devtools
+## addMutationListener
+
+It is possible to listen to all mutations performed in Overmind. This allows you to create special effects based on mutations within a certain domain of your app, or whatever else you come up with.
+
+```marksy
+h(Example, { name: "api/app_addMutationListener" })
+```
+
+
+## options.devtools
 By default the application connects to the devtools on **localhost:3031**, but you can change this in case you need to use an IP address ot the devtools has configured with a different port.
 
 ```marksy
---
+++ packages/overmind-website/examples/api/app_addMutationListener.ts
@@ -0,0 +1,38 @@
+import { tsAppIndex } from '../templates'
+
+export default (ts, view) =>
+  ts
+    ? [
+        {
+          fileName: 'app/onInitialize.ts',
+          code: `
+import { OnInitialize } from 'overmind'
+
+const onInitialize: OnInitialize = async ({ value: overmind, state, localStorage }) => {
+  overmind.addMutationListener((mutation) => {
+    if (mutation.path.indexOf('todos') === 0) {
+      localStorage.set('todos', state.todos)
+    }
+  })
+}
+
+export default onInitialize
+`,
+        },
+      ]
+    : [
+        {
+          fileName: 'app/onInitialize.js',
+          code: `
+const onInitialize = async ({ value: overmind, state, localStorage }) => {
+  overmind.addMutationListener((mutation) => {
+    if (mutation.path.indexOf('todos') === 0) {
+      localStorage.set('todos', state.todos)
+    }
+  })
+}
+
+export default onInitialize
+`,
+        },
+      ]
