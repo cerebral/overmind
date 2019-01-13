8607713b579a93776c446bc0397b3f7394a0792f
FWeinb <fabrice@weinberg.me>
2019-01-13 14:26:08 +0100

    fix(website): reduce bundle size by excluding inline source maps and add limited support for iOS10 m

--- packages/overmind-website/webpack.config.js
+++ packages/overmind-website/webpack.config.js
@@ -1,55 +1,75 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  devtool: 'inline-source-map',
-  entry: path.join(__dirname, 'src', 'index.tsx'),
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+module.exports = (env, args) => {
+  const isProduction = args.mode === 'production'
+  return {
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
     },
-  },
-  devServer: {
-    proxy: {
-      '/backend': 'http://localhost:5000',
-    },
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
+    optimization: Object.assign(
       {
-        test: /\.md/,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.(png|woff2)$/,
-        use: [
-          {
-            loader: 'url-loader',
-          },
-        ],
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        include: path.join(__dirname, 'src'),
-        options: {
-          allowTsInNodeModules: true,
+        splitChunks: {
+          chunks: 'all',
         },
       },
+      isProduction && {
+        minimizer: [
+          (compiler) => {
+            const TerserPlugin = require('terser-webpack-plugin')
+            new TerserPlugin({
+              cache: true,
+              parallel: true,
+              sourceMap: true,
+              terserOptions: {
+                safari10: true,
+              },
+            }).apply(compiler)
+          },
+        ],
+      }
+    ),
+    devServer: {
+      proxy: {
+        '/backend': 'http://localhost:5000',
+      },
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.md/,
+          use: 'raw-loader',
+        },
+        {
+          test: /\.(png|woff2)$/,
+          use: [
+            {
+              loader: 'url-loader',
+            },
+          ],
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          include: path.join(__dirname, 'src'),
+          options: {
+            allowTsInNodeModules: true,
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'backend', 'index.html'),
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'backend', 'index.html'),
-    }),
-  ],
+  }
 }
