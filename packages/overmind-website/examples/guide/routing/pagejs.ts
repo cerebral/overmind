6b7d1336a47db24a0d99deb28b4f021c857e3dc9
Cloudo <cloudo.shifa@gmail.com>
2019-02-02 21:53:10 +0300

    fix error in onInitialize

--- packages/overmind-website/examples/guide/routing/pagejs.ts
+++ packages/overmind-website/examples/guide/routing/pagejs.ts
@@ -21,7 +21,7 @@ export default onInitialize
         {
           fileName: 'overmind/onInitialize.ts',
           code: `
-const onInitialize = ({ actions, router }) => {
+const onInitialize = ({ actions, effects }) => {
   effects.router.route('/', actions.showHomePage)
   effects.router.route('/users', actions.showUsersPage)
   effects.router.route<{ id: string }>('/users/:id', actions.showUserModal)
