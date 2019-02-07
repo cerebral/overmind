9c22e4646f0389c8bcd2871c0d5470ecdc4e7bb1
Jaroslaw Zabiello <hipertracker@gmail.com>
2019-02-07 16:33:56 +0000

    Update declare.ts
    supress tslint complaining

--- packages/overmind-website/examples/guide/typescript/declare.ts
+++ packages/overmind-website/examples/guide/typescript/declare.ts
@@ -6,6 +6,7 @@ import { IConfig } from 'overmind'
 const config = {}
 
 declare module 'overmind' {
+  // tslint:disable:interface-name
   interface Config extends IConfig<typeof config> {}
 }
         `,
