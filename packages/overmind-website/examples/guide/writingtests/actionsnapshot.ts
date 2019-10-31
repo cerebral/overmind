d02fb72979eac84efc9b75af1ecb5be118e23186
Joe Hsu <jhsu.x1@gmail.com>
2019-10-31 16:22:15 -0700

    fix syntax on writing tests guide

--- packages/overmind-website/examples/guide/writingtests/actionsnapshot.ts
+++ packages/overmind-website/examples/guide/writingtests/actionsnapshot.ts
@@ -24,7 +24,7 @@ describe('Actions', () => {
     })
     test('should handle errors', async () => {
       const overmind = createOvermindMock(config, {
-        api = {
+        api: {
           getPost() {
             throw new Error('test')
           }
