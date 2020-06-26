3d16a7a928656e4e38f1a1c84204558c5fa48bb6
Christian Alfoni <christianalfoni@gmail.com>
2020-06-26 20:05:34 +0200

    feat(a-json): add new async json package

--- .cz-config.js
+++ .cz-config.js
@@ -43,6 +43,7 @@ module.exports = {
     { name: 'action-chain' },
     { name: 'proxy-state-tree' },
     { name: 'betsy' },
+    { name: 'a-json' },
   ],
 
   // it needs to match the value for field type. Eg.: 'fix'
